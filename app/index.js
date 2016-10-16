ef6c90ab80fbd287f34744736f493d0753dda82e
Darek Dziamalek <ddziamalek@advisorsacdemy.com>
2016-10-16 12:13:32 -0400

    deps

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
 // app.set('views', path.join(__dirname, '..', '/views'));
-app.set('views',__dirname + '/../views');
+app.set('views', 'views');
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({
