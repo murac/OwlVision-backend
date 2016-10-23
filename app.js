2d5ddf7a1a1b9081f285f8e1c6dd8e9471d2b2bc
Darek Dziamalek <ddziamalek@advisorsacdemy.com>
2016-10-23 17:08:37 -0400

    comment route

--- app.js
+++ app.js
@@ -17,26 +17,26 @@ var user = require('./routes/user');
 
 // This will configure Passport to use Auth0
 var strategy = new Auth0Strategy({
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+	domain: process.env.AUTH0_DOMAIN,
+	clientID: process.env.AUTH0_CLIENT_ID,
+	clientSecret: process.env.AUTH0_CLIENT_SECRET,
+	callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
 }, function (accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
+	// accessToken is the token to call Auth0 API (not needed in the most cases)
+	// extraParams.id_token has the JSON Web Token
+	// profile has all the information from the user
+	return done(null, profile);
 });
 
 passport.use(strategy);
 
 // you can use this section to keep a smaller payload
 passport.serializeUser(function (user, done) {
-    done(null, user);
+	done(null, user);
 });
 
 passport.deserializeUser(function (user, done) {
-    done(null, user);
+	done(null, user);
 });
 
 const app = express();
@@ -53,9 +53,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(session({
-    secret: 'shhhhhhhhh',
-    resave: true,
-    saveUninitialized: true
+	secret: 'shhhhhhhhh',
+	resave: true,
+	saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -70,9 +70,9 @@ app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -80,23 +80,23 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
