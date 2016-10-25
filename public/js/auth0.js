34472dd7a8fbc978cf8abbaefe78b7190adbd792
Darek Dziamalek <ddziamalek@advisorsacdemy.com>
2016-10-25 12:55:58 -0400

    auth0 callback

--- public/js/auth0.js
+++ public/js/auth0.js
@@ -1,5 +1,5 @@
 var env = {
     AUTH0_CLIENT_ID: "9obDtbvTuCrTGYPbHgdQsHNutCUGU3aJ",
     AUTH0_DOMAIN: "owlvision.auth0.com",
-    AUTH0_CALLBACK_URL: 'http://localhost:3000/callback'
+    AUTH0_CALLBACK_URL: 'https://owlvision.herokuapp.com/callback'
 };
